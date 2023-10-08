0fa080d651f43e86031667dbeddbdb57f79829d3
Fic-1 <filip.brkovic1@gmail.com>
2023-10-08 13:06:28 +0200

    express testing

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+//Defining a route
+//Route is how application response to a certain client request, to a certain url
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+}); // start up the server
