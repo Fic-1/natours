81cdd3e0f1769a100d4381a0b5c294d150dba849
Fic-1 <filip.brkovic1@gmail.com>
2023-10-11 15:33:07 +0200

    Section8@Lesson86

--- server.js
+++ server.js
@@ -19,6 +19,37 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name <Error string>'], //Validator
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
+
+const testTour = new Tour({
+  //Creating new document
+  name: 'The Park Camper',
+  price: 899,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log(err, '🤯🤯')); // save document to the database
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
