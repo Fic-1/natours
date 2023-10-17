774fbb743b08edc87644bbc468bc6131801b0348
Fic-1 <filip.brkovic1@gmail.com>
2023-10-17 11:35:41 +0200

    Section9@Lesson112

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -24,5 +19,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter); // MOUNTING THE ROUTER
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (res, req, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 // start up the server
 module.exports = app;
