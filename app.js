4f5a5bdce5d4636af4560ac703e577b86c630069
Fic-1 <filip.brkovic1@gmail.com>
2023-10-30 10:54:00 +0100

    S12@L177

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,7 +14,14 @@ const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //* 1) GLOBAL MIDDLEWARES
+//? Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //?Set security HTTP headers
 app.use(helmet());
@@ -53,9 +61,6 @@ app.use(
   }),
 );
 
-//? Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 //? Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -63,6 +68,13 @@ app.use((req, res, next) => {
   next();
 });
 //*ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Filip',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter); // MOUNTING THE ROUTER
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
