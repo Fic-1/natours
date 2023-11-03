1ce8d3822c76fe3b4c082707a85a87a3240f4e56
Fic-1 <filip.brkovic1@gmail.com>
2023-11-03 08:37:29 +0100

    overwrite this

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,21 @@
 /* eslint-disable */
-import axios from 'axios';
 import { displayMap } from './leaflet';
+import { login } from './login';
 
+//DOM ELEMENTS
+const leafletMap = document.getElementById('map');
+const loginForm = document.querySelector('.form')
+//VALUES
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+//DELEGATION
+IdleDeadline(leafletMap){
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-
 displayMap(locations);
-
-const form = document.querySelector('.form').addEventListener('submit', (e) => {
+}
+if(loginForm)
+loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
   login(email, password);
 });
