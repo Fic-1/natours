96e76e9a4709ff9ed3cc524e5106c52e7bb919af
Fic-1 <filip.brkovic1@gmail.com>
2023-11-07 14:54:17 +0100

    S13@L205

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@
 import { login, logout } from './login';
 import { displayMap } from './leaflet';
 import { updateSettings } from './updateSettings';
+import { update } from '../../models/userModel';
 
 // console.log('running index.js');
 
@@ -30,9 +31,16 @@ if (logOutBtn) logOutBtn.addEventListener('click', logout);
 if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    updateSettings({ name, email }, 'data');
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
+
+    updateSettings(form, 'data');
+    // const name = document.getElementById('name').value;
+    // const email = document.getElementById('email').value;
+    // updateSettings({ name, email }, 'data');
   });
 if (userPasswordForm)
   userPasswordForm.addEventListener('submit', async (e) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -26,8 +26,14 @@ router.get(
   userController.getMe,
   userController.getUser,
 );
-router.patch('/updateMe', userController.updateMe);
+
 router.delete('/deleteMe', userController.deleteMe);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe,
+);
 
 router.use(authController.restrictTo('admin'));
 
